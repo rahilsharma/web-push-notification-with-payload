cd1e37d323987b027fed4c85028d3558f0b31dfb
rahil sharma <liharsharma91092@gmail.com>
2016-07-12 16:55:08 +0530

    adding route folder

---
+++ routes/gcm.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log("send message to");
+
+  var subscription = '{"endpoint":"https://android.googleapis.com/gcm/send/cdOyBZ-6_CY:APA91bHWX9oF1uXgwM7kDoNr6tnHf9zWuX2SJJ4cf1g2qjzKvokBybYCYq5DJnB5asrWrYekDvAT9S_Y9RqKMtCPNf2kJz4R2j_kl8G92dP3PbqYEqaQpanlqBf4w7VSjr1ZrvuGoyh4","keys":{"p256dh":"BENro4XidqEqULgMu_X-9cx6eEfAGwXyBgdCi65A1wBF9pXhl03ZDNUTLcvQQs_yq09PpNVSDyjmM5fEAxMBkCE=","auth":"RZcJuQ2HRfq1Eank1qazgA=="}}';
+  var subs = JSON.parse(subscription);
+  var messageToSend = {'title':"Midhun Baba",'body':"Ki Jai ho",'icon':'http://talkoot.in/beta/rsz_trending.png'};
+ webpush.sendWebPush(JSON.stringify(messageToSend), subs).then(function(success){
+   console.log(success);
+ },function(err){
+   console.log(err);
+ })
+
+});
+
+module.exports = router;
