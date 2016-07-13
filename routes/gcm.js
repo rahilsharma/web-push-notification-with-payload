7437df6f6b2f3e76966b5075b8c0873284f62ad5
rahil sharma <liharsharma91092@gmail.com>
2016-07-13 16:05:49 +0530

    changing the subscription and messageToSend
    changing the subscription and messageToSend

--- routes/gcm.js
+++ routes/gcm.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   console.log("send message to");
-
-  var subscription = '{"endpoint":"https://android.googleapis.com/gcm/send/cdOyBZ-6_CY:APA91bHWX9oF1uXgwM7kDoNr6tnHf9zWuX2SJJ4cf1g2qjzKvokBybYCYq5DJnB5asrWrYekDvAT9S_Y9RqKMtCPNf2kJz4R2j_kl8G92dP3PbqYEqaQpanlqBf4w7VSjr1ZrvuGoyh4","keys":{"p256dh":"BENro4XidqEqULgMu_X-9cx6eEfAGwXyBgdCi65A1wBF9pXhl03ZDNUTLcvQQs_yq09PpNVSDyjmM5fEAxMBkCE=","auth":"RZcJuQ2HRfq1Eank1qazgA=="}}';
+  var subscription =  req.body.subscription;
+  var messageToSend = req.body.messageToSend;
+  // var subscription = '{"endpoint":"https://android.googleapis.com/gcm/send/cdOyBZ-6_CY:APA91bHWX9oF1uXgwM7kDoNr6tnHf9zWuX2SJJ4cf1g2qjzKvokBybYCYq5DJnB5asrWrYekDvAT9S_Y9RqKMtCPNf2kJz4R2j_kl8G92dP3PbqYEqaQpanlqBf4w7VSjr1ZrvuGoyh4","keys":{"p256dh":"BENro4XidqEqULgMu_X-9cx6eEfAGwXyBgdCi65A1wBF9pXhl03ZDNUTLcvQQs_yq09PpNVSDyjmM5fEAxMBkCE=","auth":"RZcJuQ2HRfq1Eank1qazgA=="}}';
+  // var messageToSend = {'title':"Midhun Baba",'body':"Ki Jai ho",'icon':'http://talkoot.in/beta/rsz_trending.png'};
   var subs = JSON.parse(subscription);
-  var messageToSend = {'title':"Midhun Baba",'body':"Ki Jai ho",'icon':'http://talkoot.in/beta/rsz_trending.png'};
  webpush.sendWebPush(JSON.stringify(messageToSend), subs).then(function(success){
    console.log(success);
  },function(err){
